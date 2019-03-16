72d463453ceea1127e95c9e290a47bc8d39aa7c6
Erin Ponsonby <erinponsonby@gmail.com>
2019-03-16 13:44:38 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,10 +32,10 @@ def hit?(current_card_total)
   prompt_user
   user_input = get_user_input
   if user_input == "S" || user_input == "s"
-    return display_card_total(current_card_total)
+    return current_card_total
   elsif user_input == "h" || user_input == "h"
     deal_card
-    return display_card_total(current_card_total)
+    return current_card_total
   elsif user_input != "s" || user_input != "h"
     invalid_command
     prompt_user
